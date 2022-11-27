c97ee15a675cc56b4ef68aba9b3830cf987479b9
Andre Koga <social@koga.one>
2022-11-27 00:30:36 -0500

    date to posts

--- app/[slug]/[pageslug]/page.tsx
+++ app/[slug]/[pageslug]/page.tsx
@@ -4,6 +4,36 @@ import { use } from "react";
 import { client } from "../../sanityClient";
 import imageUrlBuilder from "@sanity/image-url";
 
+var monthNames = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+function ordinal(i: number) {
+  var j = i % 10,
+    k = i % 100;
+  if (j == 1 && k != 11) {
+    return i + "st";
+  }
+  if (j == 2 && k != 12) {
+    return i + "nd";
+  }
+  if (j == 3 && k != 13) {
+    return i + "rd";
+  }
+  return i + "th";
+}
+
 const builder = imageUrlBuilder(client);
 
 function urlFor(source: any) {
@@ -32,10 +62,17 @@ const Page = ({ params }: { params: any }) => {
     })
   );
 
+  const createdAt = new Date(post._createdAt);
+
   return (
     <div className="contents">
       <h1 className="pt-2 font-serif">{post.title}</h1>
       <PortableText value={post.text} components={components} />
+      <time className="text-xs italic opacity-70">
+        {createdAt.getHours()}:{createdAt.getMinutes()} -{" "}
+        {monthNames[createdAt.getMonth()]} {ordinal(createdAt.getDate())},{" "}
+        {createdAt.getFullYear()}
+      </time>
     </div>
   );
 };
