8a3a82a412dfabc30792353d45bf83ca75ffd7e1
Andre Koga <social@koga.one>
2022-11-27 00:54:14 -0500

    added description to books

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { client } from "../sanityClient";
 import { NextPage } from "next";
 import { use } from "react";
 import BookPage from "../../components/BookPage";
+import { PortableText } from "@portabletext/react";
 
 const postsPerPage = 15;
 
@@ -16,16 +17,22 @@ const Page: NextPage<Props> = ({ params, searchParams }) => {
 
   if (!isNaN(parseInt(searchParams["page"]))) pageIdx = searchParams["page"];
 
-  let { pages } = use(
-    client.fetch(`*[_type == "book" && slug.current == $slug][0]{pages[]->}`, {
-      slug,
-    })
+  let { description, pages, title } = use(
+    client.fetch(
+      `*[_type == "book" && slug.current == $slug][0]{title, description, pages[]->}`,
+      {
+        slug,
+      }
+    )
   );
   pages = pages.slice(pageIdx * postsPerPage, (pageIdx + 1) * postsPerPage);
-  console.log(pages);
 
   return (
     <div className="m-1 grid grid-cols-2 gap-3 md:grid-cols-4 md:gap-4 lg:grid-cols-6">
+      <div className="prose prose-base col-span-2 max-w-none overflow-hidden rounded bg-front px-3 py-4 text-back prose-headings:font-serif prose-headings:text-back md:col-span-4 lg:col-span-6 lg:prose-lg">
+        <h1>{title}</h1>
+        <PortableText value={description} />
+      </div>
       {pages.map((post: any, idx: number) => {
         return (
           <BookPage
